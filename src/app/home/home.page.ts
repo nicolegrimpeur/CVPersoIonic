72b87d5d22833cacab4ad168be820e68599d1585
Nicolas <nicolas.barrat@student.junia.com>
2022-12-04 17:57:45 +0100

    Correction certificats

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {experiencesPro} from '../shared/informations/experiencesPro';
 import {competences} from '../shared/informations/competences';
 import {loisirs} from '../shared/informations/loisirs';
+import {certificats} from '../shared/informations/certificats';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +10,7 @@ import {loisirs} from '../shared/informations/loisirs';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public certificats = certificats;
   public experiencesPro = experiencesPro;
   public competences = competences;
   public loisirs = loisirs;
---
+++ src/app/shared/informations/certificats.ts
@@ -0,0 +1,27 @@
+export const certificats = [
+  {
+    nomLogo: 'cambridge',
+    description: 'Cambridge First Certificate, score moyen 168',
+    date: '2021'
+  },
+  {
+    nomLogo: 'voltaire',
+    description: 'Certificat Voltaire, niveau Professionnel',
+    date: '2019'
+  },
+  {
+    nomLogo: 'bafa',
+    description: 'BAFA',
+    date: '2019'
+  },
+  {
+    nomLogo: 'psc1',
+    description: 'PSC1',
+    date: '2019'
+  },
+  {
+    nomLogo: 'permis',
+    description: 'Permis B',
+    date: '2018'
+  },
+];

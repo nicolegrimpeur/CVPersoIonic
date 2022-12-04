80db357fa6ae24d3f07032f41426362b0af9901a
Nicolas <nicolas.barrat@student.junia.com>
2022-12-04 19:13:51 +0100

    Début ajout projets

---
+++ src/app/shared/informations/projetsEcole.ts
@@ -0,0 +1,76 @@
+export const projetsEcole = [
+  {
+    titre: 'Electronique 3ème année',
+    sousTitre: 'Projet d\'électronique de fin d\'année',
+    date: 'Mai / Juin 2022',
+    description: 'Réalisation d\'un capteur de température et d\'humidité envoyant ses données à une station mère. ' +
+      'Nous avons pour cela choisis nos composants, réaliser la carte électronique, ' +
+      'mais aussi écrire le code permettant de récupérer les données du capteurs pour l\'envoyer à la station mère',
+    github: 'https://github.com/nicolegrimpeur/Elec_TempHum',
+    technologies: 'C, protocole LORA, micro contrôleur'
+  },
+  {
+    titre: 'TPLC',
+    sousTitre: 'Touche Pas La Covid',
+    date: 'Janvier 2022',
+    description: '',
+    github: 'https://github.com/nicolegrimpeur/ProjetThl',
+    technologies: 'Ionic, Typescript, Fastify, MongoDB'
+  },
+  {
+    titre: 'Calculatrice graphique',
+    sousTitre: 'Projet de Théorie des langages',
+    date: 'Décembre 2021',
+    description: '',
+    github: 'https://github.com/nicolegrimpeur/ProjetThl',
+    technologies: 'C++, SFML, Bison, Flex, CMake'
+  },
+  {
+    titre: 'HomeIO',
+    sousTitre: 'CIRious Game de deuxième année sur le thème de la maison intelligente',
+    date: 'Avril / Mai 2021',
+    description: '',
+    github: 'https://github.com/Projet-CIR2/smartHome',
+    technologies: 'JavaScript, Phaser'
+  },
+  {
+    titre: 'Stratego',
+    sousTitre: 'Projet de JavaScript',
+    date: 'Mars / Avril 2021',
+    description: '',
+    github: 'https://github.com/Projet-CIR2/projetWebCIR2',
+    technologies: 'JavaScript, MySQL'
+  },
+  {
+    titre: 'Simulation d\'un marathon',
+    sousTitre: 'Projet de C++',
+    date: 'Décembre 2020',
+    description: '',
+    github: '',
+    technologies: 'C++, Visual Studio, SFML'
+  },
+  {
+    titre: 'Santé',
+    sousTitre: 'CIRious Game de deuxième année sur le thème de la santé et du handicap',
+    date: 'Avril / Mai 2020',
+    description: '',
+    github: 'https://github.com/nicolegrimpeur/projetCIR2-1920',
+    technologies: 'JavaScript, Phaser'
+  },
+  {
+    titre: 'Jeu d\'échec',
+    sousTitre: 'Projet de JavaScript',
+    date: 'Mars / Avril 2020',
+    description: '',
+    github: 'https://github.com/nicolegrimpeur/projet_js_echec',
+    technologies: 'JavaScript'
+  },
+  {
+    titre: 'Gomoku',
+    sousTitre: 'Projet de C++',
+    date: 'Décembre 2019',
+    description: '',
+    github: 'https://github.com/nicolegrimpeur/Gomoku',
+    technologies: 'C++, Visual Studio, SFML'
+  },
+];

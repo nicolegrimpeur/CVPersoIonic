0664c5c2932a737287fe9d7e7afb68465f3e535e
Nicolas <nicolas.barrat@student.junia.com>
2022-12-28 21:17:21 +0100

    Ajout animations sur l'apparition du nom

--- src/app/shared/component/header/header.component.ts
+++ src/app/shared/component/header/header.component.ts
@@ -1,18 +1,39 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Platform} from '@ionic/angular';
+import {Component, Input, OnChanges, OnInit} from '@angular/core';
+import {AnimationController, Platform} from '@ionic/angular';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnChanges {
   @Input() public afficheNom: boolean;
 
-  constructor(public platform: Platform) {
+  constructor(public platform: Platform,
+              private animation: AnimationController,) {
   }
 
   ngOnInit() {
+  }
+
+  ngOnChanges() {
+    if (this.afficheNom) {
+      this.animation.create()
+        .addElement(document.querySelector('.nom'))
+        .duration(500)
+        .keyframes([
+          {offset: 0, opacity: '0'},
+          {offset: 1, opacity: '1'}
+        ]).play().then();
+    } else {
+      this.animation.create()
+        .addElement(document.querySelector('.nom'))
+        .duration(500)
+        .keyframes([
+          {offset: 0, opacity: '1'},
+          {offset: 1, opacity: '0'}
+        ]).play().then();
+    }
   }
 
   goTo(nom: string) {

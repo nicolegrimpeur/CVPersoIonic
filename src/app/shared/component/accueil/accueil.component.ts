898f0d23734e76012f11c3fa7e628cf1048c1fd1
Nicolas <nicolas.barrat@student.junia.com>
2022-12-28 13:12:42 +0100

    Correction accueil

--- src/app/shared/component/accueil/accueil.component.ts
+++ src/app/shared/component/accueil/accueil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Platform} from '@ionic/angular';
 
 @Component({
   selector: 'app-accueil',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccueilComponent implements OnInit {
 
-  constructor() { }
+  constructor(public platform: Platform) { }
 
   ngOnInit() {}
 

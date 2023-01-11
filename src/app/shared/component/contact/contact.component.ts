9ab2a151a5a6c11fa40a59e27b40f549936195bc
Nicolas <nicolas.barrat@student.junia.com>
2023-01-11 23:26:11 +0100

    Modification affichage du contact

--- src/app/shared/component/contact/contact.component.ts
+++ src/app/shared/component/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {contact} from '../../informations/contact';
 
 @Component({
@@ -6,18 +6,49 @@ import {contact} from '../../informations/contact';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss'],
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent implements AfterViewInit {
+  @ViewChild('h5') h5;
+
   public contact = contact;
   public affiche = false;
 
   constructor() {
   }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    const texte = 'Cliquez pour afficher';
+    for (const el of Array.from(document.getElementsByClassName('text') as HTMLCollectionOf<HTMLElement>)) {
+      el.innerHTML = texte;
+      el.style.setProperty('--characters', texte.length.toString());
+    }
   }
 
-  click() {
-    this.affiche = true;
+  testMailOrTel(texte: string) {
+    return ['Téléphone', 'Mail'].find(el => el === texte);
+  }
+
+  // au clic, on enlève les lettres, puis on met les nouvelles
+  click(event, texte: string) {
+    // suppression du texte et ajout de la barre à côté
+    event.target.parentElement.classList.add('typewriter-effect-blink');
+    event.target.className = 'text-reverse';
+
+    // une fois le texte supprimé, on affiche le nouveau texte
+    setTimeout(() => {
+      event.target.innerHTML = texte;
+      event.target.style.setProperty('--characters', 0);
+      event.target.style.setProperty('max-width', 0);
+      event.target.style.setProperty('white-space', 'nowrap');
+      event.target.className = 'text';
+      event.target.style.setProperty('--characters', texte.length.toString());
+
+      // on enlève la barre à côté une fois l'animation terminée
+      setTimeout(() => {
+        event.target.style.setProperty('max-width', '100%');
+        event.target.style.setProperty('white-space', 'normal');
+        event.target.parentElement.classList.remove('typewriter-effect-blink');
+      }, 2000);
+    }, 2000);
   }
 
 }

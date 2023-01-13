8c6ca1ee4061cb7692a1237f2f73d1427bb07d80
Nicolas <nicolas.barrat@student.junia.com>
2023-01-13 11:28:04 +0100

    Correction affichage contact

--- src/app/shared/component/contact/contact.component.ts
+++ src/app/shared/component/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import {contact} from '../../informations/contact';
 
 @Component({
@@ -7,8 +7,6 @@ import {contact} from '../../informations/contact';
   styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements AfterViewInit {
-  @ViewChild('h5') h5;
-
   public contact = contact;
   public affiche = [];
 
@@ -16,42 +14,35 @@ export class ContactComponent implements AfterViewInit {
   }
 
   ngAfterViewInit() {
-    const texte = 'Cliquez pour voir les informations';
-    for (const el of Array.from(document.getElementsByClassName('text') as HTMLCollectionOf<HTMLElement>)) {
-      el.innerHTML = texte;
-      el.style.setProperty('--characters', texte.length.toString());
-    }
   }
 
   testMailOrTel(texte: string) {
     return ['Téléphone', 'Mail'].find(el => el === texte);
   }
 
+  // fonction pour faire une pause
+  delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
   // au clic, on enlève les lettres, puis on met les nouvelles
-  click(event, texte: string) {
+  async click(event, texte: string) {
     if (!this.affiche.includes(texte)) {
       this.affiche.push(texte);
 
-      // suppression du texte et ajout de la barre à côté
-      event.target.parentElement.classList.add('typewriter-effect-blink');
-      event.target.className = 'text-reverse';
+      // on enlève les lettres
+      const tempsSuppression = 2000 / (event.target.innerHTML.length + 50);
+      for (const lettre of event.target.innerHTML) {
+        event.target.innerHTML = event.target.innerHTML.substring(0, event.target.innerHTML.length - 1);
+        await this.delay(tempsSuppression);
+      }
 
-      // une fois le texte supprimé, on affiche le nouveau texte
-      setTimeout(() => {
-        event.target.innerHTML = texte;
-        event.target.style.setProperty('--characters', 0);
-        event.target.style.setProperty('max-width', 0);
-        event.target.style.setProperty('white-space', 'nowrap');
-        event.target.className = 'text';
-        event.target.style.setProperty('--characters', texte.length.toString());
-
-        // on enlève la barre à côté une fois l'animation terminée
-        setTimeout(() => {
-          event.target.style.setProperty('max-width', '100%');
-          event.target.style.setProperty('white-space', 'normal');
-          event.target.parentElement.classList.remove('typewriter-effect-blink');
-        }, 2000);
-      }, 2000);
+      // on met les nouvelles lettres
+      const tempsAjout = 2000 / (texte.length + 50);
+      for (const lettre of texte) {
+        event.target.innerHTML += lettre;
+        await this.delay(tempsAjout);
+      }
     }
   }
 

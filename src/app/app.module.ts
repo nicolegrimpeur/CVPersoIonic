85257a1252c6100dcd920a9ca94156c87a84630e
Nicolas <nicolas.barrat@student.junia.com>
2023-01-29 17:38:20 +0100

    Ajout suivi matomo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouteReuseStrategy} from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+
+import {MatomoModule} from 'ngx-matomo';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-    providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    MatomoModule.forRoot({
+      trackers: [
+        {
+          trackerUrl: 'https://nicob.ovh/matomo/',
+          siteId: 3,
+        }
+      ],
+      routeTracking: {
+        enable: true,
+      }
+    })],
+  providers: [{provide: RouteReuseStrategy, useClass: IonicRouteStrategy}],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}

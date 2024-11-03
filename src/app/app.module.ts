cae3e08bd4c795891360ea9c79805c1e3ca6c552
Nicolas B. <nicolegrimpeur@gmail.com>
2024-11-03 17:42:38 +0100

    Revert "Mise à jour des informations et des dépendances"
    This reverts commit 2b787f777ffb14c2b761a5505ff2ccf12864704a.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 
+import {provideMatomo} from 'ngx-matomo-client';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{provide: RouteReuseStrategy, useClass: IonicRouteStrategy}],
+  providers: [{provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+    provideMatomo({trackerUrl: 'https://nicob.ovh/matomo/', siteId: '3'})],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/shared/informations/formations.ts
+++ src/app/shared/informations/formations.ts
@@ -6,9 +6,18 @@ export const formations = [
     lieu: 'Lille',
     description : [
       'Diplôme d’ingénieur CTI',
-      'Cycle ingénieur spécialité Informatique, Réseaux et Cybersécurité',
-      'Major de la promotion 65 (2024)'
+      'Cycle ingénieur spécialité Informatique, Réseaux et Cybersécurité'
     ],
     url: 'https://www.junia.com/fr/formations-admissions/nos-domaines/numerique/domaine-cybersecurite/'
+  },
+  {
+    titre : 'Lycée du Parc',
+    image: 'Lycee-parc.png',
+    dates: '2015 - 2018',
+    lieu: 'Lyon',
+    description : [
+      'Préparation au Baccalauréat Général, filière Scientifique. Option Latin et Informatique'
+    ],
+    url: ''
   },
 ];

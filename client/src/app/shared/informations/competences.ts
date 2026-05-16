af475e4cf5322fe28e78cb1452f4857ef3042c0f
Nicolas Barrat <nbarrat+maxxing@maxxing.com>
2026-05-17 00:02:22 +0200

    Nettoyage et mise au propre des compétences

--- client/src/app/shared/informations/competences.ts
+++ client/src/app/shared/informations/competences.ts
@@ -1,34 +1,26 @@
 export const competences = [
   {
     titre: 'DevSecOps',
-    image: 'web',
   },
   {
     titre: 'Kubernetes & Docker',
-    image: 'jupyter',
   },
   {
     titre: 'Administration système Linux',
-    image: 'linux',
   },
   {
     titre: 'Monitoring & observabilité',
-    image: 'teamwork',
   },
   {
     titre: 'Tests d\'intrusion & audit',
-    image: 'problem solving',
   },
   {
     titre: 'Gouvernance SSI & conformité',
-    image: 'certification',
   },
   {
     titre: 'Sécurisation réseau et applicatifs',
-    image: 'perseverance',
   },
   {
     titre: 'Conception d\'infrastructures',
-    image: 'curiosity',
   },
 ];
